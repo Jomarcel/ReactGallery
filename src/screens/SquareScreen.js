4979a920488fe87a8a3fce97e03f07d7b6ae6f7b
whyplaychess <jomarcel_n@outlook.com>
2019-12-02 11:04:10 +1000

    color change

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,13 +7,37 @@ const SquareScreen = ({color}) => {
     const [red, setRed] = useState (0);
     const [blue, setBlue] = useState (0);
     const [green, setGreen] = useState (0);
+
+    const setColor = (color, change)  =>{
+      // color === 'red', 'green', 'blue'
+      //change === + 15, -15
+      switch(color){
+        case 'red':
+          red + change > 255 || red + change < 0 ? null: setRed (red + change)
+            return;
+
+        case 'green':
+            green + change > 255 || green + change < 0 ? null: setRed (green + change)
+              return;
+
+        case 'blue':
+            blue+ change > 255 || blue + change < 0 ? null: setRed (blue + change)
+              return;
+              }
+
+              
+
+    };
 console.log(red)
     return <View>
     <Text>{color}</Text>
     <ColourCounter 
-    onIncrease = {() => setRed (red + COLOR_INCREMENT)} 
-    onDecrease = {() => setRed (red - COLOR_INCREMENT)} 
-    color = "Red" />
+    color = "red" 
+    onIncrease = {() => 
+      setColor ('red', COLOR_INCREMENT)} 
+    onDecrease = {() => 
+      setColor ('red', - 1 * COLOR_INCREMENT)} 
+    />
     <ColourCounter 
     onIncrease = {() => setGreen (green + COLOR_INCREMENT)} 
     onDecrease = {() => setGreen (green - COLOR_INCREMENT)} 
